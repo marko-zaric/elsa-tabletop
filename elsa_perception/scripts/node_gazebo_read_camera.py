b423561898e501b375c49d4f68391609982d5d2a
Erwan Renaudo <erwan.renaudo@uibk.ac.at>
2023-02-14 11:19:20 +0100

    Publishing FullScene msg (older pubs to be removed later)

--- elsa_perception/scripts/node_gazebo_read_camera.py
+++ elsa_perception/scripts/node_gazebo_read_camera.py
@@ -7,7 +7,7 @@ import sensor_msgs.point_cloud2 as pc2
 import ctypes
 import struct
 import numpy as np
-from elsa_perception_msgs.msg import PhysicalScene, PhysicalSceneStamped
+from elsa_perception_msgs.msg import PhysicalScene, PhysicalSceneStamped, FullSceneStamped, SocialFeatures
 from matplotlib import colors
 import std_msgs.msg
 
@@ -27,6 +27,7 @@ def listener():
     rospy.Subscriber("/downsample/output", PointCloud2, callback=callback)
     pub = rospy.Publisher('/scene_description', PhysicalScene, queue_size=1)
     scene_stamped_pub = rospy.Publisher('/scene_description_stamped', PhysicalSceneStamped, queue_size=1)
+    fullscene_stamped_pub = rospy.Publisher('/fullscene', FullSceneStamped, queue_size=1)
     seq_number = 0
     while not rospy.is_shutdown():
         rospy.wait_for_message("/downsample/output", PointCloud2)
@@ -80,9 +81,18 @@ def listener():
             SCENE_stamped.header = std_msgs.msg.Header()
             SCENE_stamped.header.stamp = rospy.Time.now()
             SCENE_stamped.header.seq = seq_number
-            seq_number += 1
+            
+            FULLSCENE_stamped = FullSceneStamped()
+            FULLSCENE_stamped.physical_scene = SCENE.physical_scene
+            FULLSCENE_stamped.header = std_msgs.msg.Header()
+            FULLSCENE_stamped.header.stamp = SCENE_stamped.header.stamp
+            FULLSCENE_stamped.header.seq = SCENE_stamped.header.seq
+            FULLSCENE_stamped.social_features.append(SocialFeatures())
+
             pub.publish(SCENE)
             scene_stamped_pub.publish(SCENE_stamped)
+            fullscene_stamped_pub.publish(FULLSCENE_stamped)
+            seq_number += 1
     
 
 if __name__ == '__main__':
