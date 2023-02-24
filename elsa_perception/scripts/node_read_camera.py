c3ffa939359a30f7cc1658668ec35b7314051c8b
Marko Zaric <marko.zaric@uibk.ac.at>
2023-02-24 17:50:58 +0100

    renewed launch for realsense camera with up to date packages

--- elsa_perception/scripts/node_read_camera.py
+++ elsa_perception/scripts/node_read_camera.py
@@ -1,34 +1,19 @@
 import rospy
-from perception.pointcloud_bb import pointcloudBB
+# from perception.pointcloud_bb import pointcloudBB
 from sensor_msgs.point_cloud2 import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 import ctypes
 import struct
 import numpy as np
-from elsa_perception_msgs.msg import BoundingBox
-from elsa_perception_msgs.msg import BB_Scene
+from matplotlib import colors
+from elsa_perception_msgs.msg import PhysicalScene, ClusteredPointcloud
 # sensor_msgs/PointCloud2
 
-arrayBBS = None
 SCENE = None
 DATA_CALLBACK = None
-
-def ArrayToBBScene(bounding_boxes):
-    scene = []
-    for obj in bounding_boxes:
-        bounding_box = BoundingBox()
-        bounding_box.x = obj[0]
-        bounding_box.y = obj[1]
-        bounding_box.z = obj[2]
-        bounding_box.phi = obj[3]
-        bounding_box.dx = obj[4]
-        bounding_box.dy = obj[5]
-        bounding_box.dz = obj[6]
-        scene.append(bounding_box)
-    bb_scene = BB_Scene()
-    bb_scene.boundingboxes = scene
-    return bb_scene
-
+ENABLE_COLOR = True
+SAVE_POINT_CLOUD = True
+HSV_COLOR = True
 
 def callback(data):
     global DATA_CALLBACK
@@ -47,41 +32,51 @@ def callback(data):
 
 def listener():
     rospy.init_node("read_cam_data", anonymous=True)
-    rospy.Subscriber("/downsample/output", PointCloud2, callback=callback) # /downsample/output
-    pub = rospy.Publisher('/bounding_boxes', BB_Scene, queue_size=1)
-    # s = rospy.Service('observe', Observe, handle_observe)
+    rospy.Subscriber("/downsample/output", PointCloud2, callback=callback) # 
+    pub = rospy.Publisher('elsa_perception/scene_description', PhysicalScene, queue_size=1)
+    pub_cluster = rospy.Publisher('elsa_perception/clustered_pointcloud', ClusteredPointcloud , queue_size=1)
     while not rospy.is_shutdown():
         rospy.wait_for_message("/downsample/output", PointCloud2)
         gen = pc2.read_points(DATA_CALLBACK, skip_nans=True, field_names=("x", "y", "z", "rgb"))
         count_points = 0
         xyz = np.zeros((int(DATA_CALLBACK.height * DATA_CALLBACK.width), 3))
-        rgb = np.zeros((int(DATA_CALLBACK.height * DATA_CALLBACK.width), 3))
+        color = np.zeros((DATA_CALLBACK.width, 3))
         for point in gen:
-            rgb_float = point[3]
-            # # cast float32 to int so that bitwise operations are possible
-            s = struct.pack('>f' ,rgb_float)
-            i = struct.unpack('>l',s)[0]
+            if ENABLE_COLOR:
+                rgb_float = point[3]
+                # # cast float32 to int so that bitwise operations are possible
+                s = struct.pack('>f' ,rgb_float)
+                i = struct.unpack('>l',s)[0]
 
 
-            # # you can get back the float value by the inverse operations
-            pack = ctypes.c_uint32(i).value
-    
-            r = (pack & 0x00FF0000)>> 16
-            g = (pack & 0x0000FF00)>> 8
-            b = (pack & 0x000000FF)
+                # # you can get back the float value by the inverse operations
+                pack = ctypes.c_uint32(i).value
 
-            xyz[count_points, :3] = [point[0], point[1], point[2]]
-            rgb[count_points, :3] = [r, g, b]
+                r = (pack & 0x00FF0000)>> 16
+                g = (pack & 0x0000FF00)>> 8
+                b = (pack & 0x000000FF)
+
+                xyz[count_points, :3] = [point[0], point[1], point[2]]
+
+                if HSV_COLOR:
+                    color[count_points, :3] = colors.rgb_to_hsv([r / 255, g / 255, b / 255])
+                else: 
+                    color[count_points, :3] = [r, g, b]
 
             count_points += 1
         print(count_points)
-        np.save("/home/marko/Desktop/IIS_Research/xyz_real.npy", xyz)
-        np.save("/home/marko/Desktop/IIS_Research/rgb_real.npy", rgb)
-        print("saved")
+        if SAVE_POINT_CLOUD:
+            np.save("/home/marko/IIS/point_clouds/xyz_real.npy", xyz)
+            if ENABLE_COLOR:
+                if HSV_COLOR:
+                    np.save("/home/marko/IIS/point_clouds/hsv.npy", color)
+                else:
+                    np.save("/home/marko/IIS/point_clouds/rgb.npy", color)
+            print("saved")
 
-        SCENE = ArrayToBBScene(pointcloudBB(xyz))
+        # SCENE = ArrayToBBScene(pointcloudBB(xyz))
         
-        pub.publish(SCENE)
+        # pub.publish(SCENE)
     
 
 if __name__ == '__main__':
